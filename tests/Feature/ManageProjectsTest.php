fd8d53e4a3419d5a60f4ef45ed182af59a8615bf
ezzamel <ezzamel156@gmail.com>
2020-01-13 20:49:55 +0800

    First commit. Done til ep 7

---
+++ tests/Feature/ManageProjectsTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectsTest extends TestCase
+{
+
+    use WithFaker, RefreshDatabase;
+
+    /** @test */
+    
+    public function guests_cannot_manage_projects()
+    {
+
+        //$this->withoutExceptionHandling();
+
+        $project = factory('App\Project')->create();
+       
+        $this->get('/projects')->assertRedirect('login');
+
+        $this->get($project->path())->assertRedirect('login');
+
+        $this->post('/projects', $project->toArray())->assertRedirect('login');
+    
+    }
+
+    /** @test */
+    
+    public function a_user_can_create_a_project()
+    {
+
+        $this->withoutExceptionHandling();
+
+        $this->actingAs(factory('App\User')->create()); //sign in user
+
+        $this->get('/projects/create')->assertStatus(200);
+
+        $attributes = [
+
+            'title' => $this->faker->sentence,
+
+            'description' => $this->faker->paragraph
+
+        ];
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+
+    }
+
+    /** @test */
+    
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+
+        $this->actingAs(factory('App\User')->create()); //sign in user
+
+        //$this->withoutExceptionHandling();
+
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertStatus(403);
+
+    }
+
+    /** @test */
+    
+    public function a_user_can_view_their_project() 
+    {
+
+        $this->actingAs(factory('App\User')->create()); //sign in user
+
+        $this->withoutExceptionHandling();
+
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
+
+        $this->get($project->path())
+            ->assertSee($project->description)
+            ->assertSee($project->title);
+    }
+
+    /** @test */
+    
+    public function a_project_requires_a_title()
+    {
+        $this->actingAs(factory('App\User')->create()); //sign in user
+
+        $attributes = factory('App\Project')->raw(['title' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+
+    }
+
+    /** @test */
+    
+    public function a_project_requires_a_description()
+    {
+        $this->actingAs(factory('App\User')->create()); //sign in user
+
+        $attributes = factory('App\Project')->raw(['description' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
+
+    }
+
+
+    
+}
