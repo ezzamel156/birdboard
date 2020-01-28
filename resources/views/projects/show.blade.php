ace5009085d880f15bf5d8a26027fd8666949c7d
ezzamel <ezzamel156@gmail.com>
2020-01-28 23:59:15 +0800

    Created partical for activity cards. Implemented polymorphism in view

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -50,7 +50,6 @@
                     {{-- general notes --}}
 
                     <form action="{{ $project->path() }}" method="POST">
-
                         @csrf
                         @method('PATCH')
                     
@@ -62,22 +61,15 @@
                         >{{$project->notes}}</textarea>
 
                         <button type="submit" class="button">Save</button>
-                    
                     </form> 
-                    @if ($errors->any())
-                        <div class="field mt-6">    
-                                @foreach ($errors->all() as $error)
-                                    <li class="text-sm text-red"> {{ $error }} </li>            
-                                @endforeach    
-                        </div>
-                    @endif                   
+                    
+                    @include('errors')
                 </div>
             </div>
 
             <div class="lg:w-1/4 px-3 lg:py-8">
-
-                @include('projects.card')      
-
+                @include('projects.card')           
+                @include('projects.activity.card')
             </div>
         </div>
     </main>
