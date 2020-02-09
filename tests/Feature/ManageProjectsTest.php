1748c2e08fdac60ad81423a61e61169e664112bd
ezzamel <ezzamel156@gmail.com>
2020-02-09 19:37:15 +0800

    Done episode 29. Add delete method for project, project delete feature test. Used route resource

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -64,9 +64,39 @@ class ManageProjectsTest extends TestCase
 
     /** @test */
     
-    public function a_user_can_update_a_project()
+    public function unauthorized_users_cannot_delete_a_project()
+    {
+        $project = ProjectFactory::create();
+    
+        $this->delete($project->path())
+            ->assertRedirect('/login');
+
+        $this->signIn();
+
+        $this->delete($project->path())->assertStatus(403);
+
+        //$this->assertDatabaseMissing('projects', $project->only('id'));
+    }
+
+    /** @test */
+    
+    public function a_user_can_delete_a_project()
     {
         $this->withoutExceptionHandling();
+        $project = ProjectFactory::create();
+    
+        $this->ActingAs($project->owner)
+            ->delete($project->path())
+            ->assertRedirect('/projects');
+
+        $this->assertDatabaseMissing('projects', $project->only('id'));
+    }
+
+    /** @test */
+    
+    public function a_user_can_update_a_project()
+    {
+        //$this->withoutExceptionHandling();
         $project = ProjectFactory::create();
     
         $this->ActingAs($project->owner)
