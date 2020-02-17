9e45eeb3f486032f0361da2e31024e5d9852a189
ezzamel <ezzamel156@gmail.com>
2020-02-17 20:37:06 +0800

    correction in projects.show

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -11,13 +11,13 @@
             <div class="flex items-center">
                 @foreach ($project->members as $member)
                     <img 
-                        src="{{ gravatar_url($member->email)."&d='https://s3.amazonaws.com/laracasts/images/default-square-avatar.jpg'" }}" 
+                        src="{{ gravatar_url($member->email) }}" 
                         alt=" {{ $member->name }}'s avatar " 
                         class="rounded-full w-8 mr-2">
                 @endforeach
 
                 <img 
-                    src="{{ gravatar_url($project->owner->email)."&d='https://s3.amazonaws.com/laracasts/images/default-square-avatar.jpg'" }}" 
+                    src="{{ gravatar_url($project->owner->email) }}" 
                     alt=" {{ $project->owner->name }}'s avatar " 
                     class="rounded-full w-8 mr-2">
 
