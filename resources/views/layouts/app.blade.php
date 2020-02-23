bac462d7521834ad5adf48b5213ab8d2cfaf16db
ezzamel <ezzamel156@gmail.com>
2020-02-23 11:37:38 +0800

    Changed nav header url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <div class="container mx-auto">
                 <div class="flex justify-between items-center py-2">
                     <h1>
-                        <a class="navbar-brand" href="{{ url('/') }}">
+                        <a class="navbar-brand" href="{{ url('/projects') }}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="291" height="45" viewBox="0 0 291 45" class="text-default relative" style="top: 2px">
                                 <g fill="none" fill-rule="evenodd">
                                     <g class="fill-current">
